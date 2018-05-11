1eb73759646a96328ca9720d4151e0a26540594a
Aurelius Calliste <callicodes@gmail.com>
2018-05-11 15:45:09 +0100

    Added project files

---
+++ question2.js
@@ -0,0 +1,22 @@
+
+var radio0 = document.getElementById("budget-0");
+radio0.addEventListener("click", function(){
+alert("Someone Clicked The Button!");});
+
+
+
+var radio1 = document.getElementById("budget-1");
+radio1.addEventListener("click", function(){
+alert("Someone Clicked The Button!");});
+
+
+
+var radio2 = document.getElementById("budget-2");
+radio2.addEventListener("click", function(){
+alert("Someone Clicked The Button!");});
+
+
+
+var radio3 = document.getElementById("budget-3");
+radio3.addEventListener("click", function(){
+alert("Someone Clicked The Button!");});
---
+++ question3.js
@@ -0,0 +1,63 @@
+var firstName = document.getElementById('firstName').value;
+var lastName = document.getElementById('lastName').value;
+var tag2 = document.getElementById("lastNameField");
+var tag3 = document.getElementsByClassName("o-forms__label")[3];
+
+
+fullName = firstName + " " + lastName;
+
+tag3.textContent = 'Name*';
+
+
+var tag = document.getElementById("firstNameField");
+
+
+
+
+
+
+
+
+tag.addEventListener('click', () => {
+
+let input  = document.createElement('input')
+
+
+
+
+
+
+
+
+});
+
+
+tag.textContent = fullName;
+
+tag2.style.display = "none";
+
+tag.style.border = "1px solid #b3a9a0";
+
+tag.style.background = "#fff";
+
+tag.style.padding = "5px";
+
+tag.style.width = "222px";
+
+
+
+let name = document.createElement('h1');
+
+name = document.getElementsByClassName("o-forms__label")[3];
+
+name.textContent = 'Name*';
+
+name.style.right = "241px";
+
+
+tag2.style.display = "inline";
+
+var inputter = document.getElementById("lastName");
+
+inputter.style.display = "none";
+
